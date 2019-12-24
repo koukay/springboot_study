511a6ee42084698d8cbf0b600aa4bc520fd44cfa
houkai <hikoukay@163.com>
2019-12-24 22:24:55 +0800

    1

---
+++ src/test/java/com/houkai/SpringbootStudysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.houkai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootStudysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
